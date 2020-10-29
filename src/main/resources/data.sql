4aa762bf36eca0feb4580b2ad0bb9d712604e874
Rebecca <rlwatkins345@gmail.com>
2020-10-29 18:50:36 -0500

    database set up

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
-INSERT INTO person (name) VALUES
-  ('Ryan'),
-  ('Becca'),
-  ('Jesse'),
-  ('Heather'),
-  ('Mike'),
-  ('Deb');
+MERGE INTO person (id, name) VALUES
+  (1, 'Ryan'),
+  (2, 'Becca'),
+  (3, 'Jesse'),
+  (4, 'Heather'),
+  (5, 'Mike'),
+  (6, 'Deb');
